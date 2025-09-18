6668d2aa9c3016bf4e34c4de1e905ad53c7811a3
raoParam <paramrao2904@gmail.com>
2025-09-18 12:00:10 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 // App.js
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Home from './Components/home';
+import Home from '../Components/home';
 import DocsPage from '../Docs/DocsPage';
 import ChatInterface from './Components/chatInterface';
 import Layout from './Components/Layout';
